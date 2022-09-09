173c3b37f3651294f2830548a1d507ddc85509b9
DavidTatis <ec2-user@ip-172-31-55-34.ec2.internal>
2022-09-09 21:45:38 +0000

    Lab2 demo code fixed

--- lab-2.py
+++ lab-2.py
@@ -1,12 +1,14 @@
 
-operations_count = 0
+
 
 def main():
+    operations_count = 0
     ask_again = True
     while(ask_again):
         a = input("Enter the numerator: ")
         b = input("Enter the denominator: ")
         result = perform_division(a,b)
+        operations_count += 1
         print(result)
         ask_again = input("Do you want to perform another operation? Enter yes or no: ")
         if(ask_again == 'yes'):
@@ -17,12 +19,13 @@ def main():
 
 
 def perform_division(a,b):
-    global operations_count
+    
     try:
-        operations_count += 1
         return int(a)/int(b)
+    except ZeroDivisionError as e:
+        print("Zero division Error")
     except Exception as e:
-        pass
+        print("exception")
 
 
 main()
